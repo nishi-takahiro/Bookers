19af500da54957dcdc07284c90cec2958f790afc
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-05-02 07:36:56 +0000

    id error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,22 +5,26 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if @book.save
-     redirect_to '/books/:id'
-   else
-     render :new
-  end
-end
+    @book.save
+     redirect_to book_path(@book.id)
+ end
 
   def index
     @books = Book.all
   end
 
   def show
-   
+   @book = Book.find(params[:id])
   end
 
   def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   def destroy
